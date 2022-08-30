c72665b9eff5d2ca16109cb344fdcd490983454c
Th0m4s <thomasdavalos@outlook.com>
2022-08-30 20:39:20 -0300

    Resuelto 30/08/2022

---
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_8.py
@@ -0,0 +1,6 @@
+
+
+numero = int(input("Ingresar numero para mostrar su tabla: \n"));
+
+for multiplicador in range(1,11):
+    print(f"{numero} x {multiplicador} = {numero * multiplicador}")
---
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_9.py
@@ -0,0 +1,12 @@
+
+dividendo = float(input("Ingresar dividendo:"));
+divisor = float(input("Ingresar divisor: "));
+
+print("\n========Resultado========")
+print(f"\nDivisión = {dividendo / divisor} \n")
+print(f"Cociente = {dividendo // divisor} \n")
+print(f"Resto = {dividendo % divisor}")
+
+
+
+
