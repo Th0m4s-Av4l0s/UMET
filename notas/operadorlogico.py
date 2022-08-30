13082d86ed49e971c4b0354035476e35cfb8f1f0
Th0m4s-Av4l0s <101676358+Th0m4s-Av4l0s@users.noreply.github.com>
2022-08-30 19:56:45 -0300

    Delete notas directory

--- notas/operadorlogico.py
+++
@@ -1,6 +0,0 @@
-#Operadores logicos para no confundir con JAVA
-#AND = y (True = si ambos valores son True)
-#OR = (True = si algun solo valor es True)
-#NOT = (True = si algun valor es False)
-# EJ de NOT: "4 == 5 es false en booleano"
-# "not 4 == 5 es true en booleano "
--- notas/saltolinea.py
+++
@@ -1,6 +0,0 @@
-#con \n se puede hacer un doble salto de
-#linea para adornar un programa.
-
-print("========= \n")
-print("saludo \n")
-print("==========")
