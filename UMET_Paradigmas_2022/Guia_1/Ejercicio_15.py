ea0cfe677e49adddff26340c1d796d636c451c69
Th0m4s <thomasdavalos@outlook.com>
2022-09-01 16:29:13 -0300

    Resuelto, falta comentar

---
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_15.py
@@ -0,0 +1,20 @@
+#Mismo ejercicio que el "Ejercicio_14.py"
+num_1 = 0
+num_2 = 0
+num_3 = 0
+resultado = 0
+
+num_1 = int(input(">Ingresar primer numero a sumar: "));
+num_2 = int(input(">Ingresar segundo numero a sumar: "));
+num_3 = int(input(">Ingresar tercer numero a sumar: "));
+
+resultado = num_1 + num_2 + num_3;
+print(f"\n///El resultado de la suma es = {resultado}///");
+
+if(resultado > 10):
+    print("\nEl resultado es mayor a 10, debe dividirse por 2.");
+    print(f"{resultado} // 2 = {resultado // 2}");
+
+elif(resultado < 10):
+    print("El resultado es menor a 10, debe potenciarse al cubo.");
+    print(f"{resultado} ** 3 = {resultado ** 3}")
