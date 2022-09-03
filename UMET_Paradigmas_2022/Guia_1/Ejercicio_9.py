cdbfba5ca4f793c8eea77b1addd6e00f2d664ec8
Th0m4s <thomasdavalos@outlook.com>
2022-09-03 12:56:40 -0300

    Resuelto

--- UMET_Paradigmas_2022/Guia_1/Ejercicio_9.py
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_9.py
@@ -1,7 +1,14 @@
+#Declaracion de variables
+dividendo = 0.0
+divisor = 0.0
 
+
+#Ingreso de datos
 dividendo = float(input("Ingresar dividendo:"));
 divisor = float(input("Ingresar divisor: "));
 
+
+#Salida de datos de la division, cociente y resto
 print("\n========Resultado========")
 print(f"\nDivisión = {dividendo / divisor} \n")
 print(f"Cociente = {dividendo // divisor} \n")
