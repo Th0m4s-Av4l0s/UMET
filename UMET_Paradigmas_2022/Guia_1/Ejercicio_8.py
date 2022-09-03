6eab48ccd109dd990a3dd05049647d5c7ee21b69
Th0m4s <thomasdavalos@outlook.com>
2022-09-03 12:54:25 -0300

    Resuelto

--- UMET_Paradigmas_2022/Guia_1/Ejercicio_8.py
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_8.py
@@ -1,7 +1,15 @@
+#Declaracion de variables
+numero = 0;
+multiplicador = 0;
 
 
+#Aca se ingresa un valor entero a la variable 'numero'
 numero = int(input("Ingresar numero para mostrar su tabla: \n"));
 
+
+#La variable 'multiplicador' al concatenar su valor 
+# gradualmente, permite imprimir el producto del numero
+# ingresado y el multiplicador de forma dinamica
 for multiplicador in range(1,11):
     print(f"{numero} x {multiplicador} = {numero * multiplicador}")
 
