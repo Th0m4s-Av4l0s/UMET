3214a3a0a29c9b900cc03d90e6f9b343740aebd1
Th0m4s <thomasdavalos@outlook.com>
2022-09-03 12:17:15 -0300

    Resuelto

--- UMET_Paradigmas_2022/Guia_1/Ejercicio_15.py
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_15.py
@@ -1,16 +1,25 @@
-#Mismo ejercicio que el "Ejercicio_14.py"
+#Mismo ejercicio que el "Ejercicio_15.py"
+
+#Declaracion de variables
 num_1 = 0
 num_2 = 0
 num_3 = 0
 resultado = 0
 
+
+#Ingreso de datos
 num_1 = int(input(">Ingresar primer numero a sumar: "));
 num_2 = int(input(">Ingresar segundo numero a sumar: "));
 num_3 = int(input(">Ingresar tercer numero a sumar: "));
 
+
+#Concatenacion de los valores ingresados
 resultado = num_1 + num_2 + num_3;
 print(f"\n///El resultado de la suma es = {resultado}///");
 
+
+#Dependiendo el valor del resultado se divide por 2 o se
+# potencia al cubo
 if(resultado > 10):
     print("\nEl resultado es mayor a 10, debe dividirse por 2.");
     print(f"{resultado} // 2 = {resultado // 2}");
