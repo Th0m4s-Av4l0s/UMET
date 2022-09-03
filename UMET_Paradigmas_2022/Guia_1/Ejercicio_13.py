a313df66ec60d84c7e2409bb93e0b9e2b4148aff
Th0m4s <thomasdavalos@outlook.com>
2022-09-03 12:12:28 -0300

    Resuelto

--- UMET_Paradigmas_2022/Guia_1/Ejercicio_13.py
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_13.py
@@ -1,18 +1,27 @@
-from tokenize import String
+
+#Declaracion de variables
+nombre = " ";
+apellido = " ";
+dominio = " ";
+email = " ";
 
 
-nombre = String
-apellido = String
-dominio = String
-email = String
-
+#Este while controla que el dominio sea correcto
 while(email.endswith("@umet.edu.ar") != True):
 
+
+    #Ingreso de caracteres para crear el email
     nombre = input("Ingresar un nombre: ");
     apellido = input("Ingresar un apellido: ");
     dominio = input("Ingresar dominio: ");
+
+    #Aca suma los 3 datos ingresados
     email = nombre + apellido + dominio;
 
+
+    #En caso de que el dominio sea incorrecto se repetira
+    # el ingreso de datos, caso contrario el programa 
+    # devuelve el email y termina.
     if(email.endswith("@umet.edu.ar") == False):
        
         print("\n===¡Error!: asegurarse de que el email contenga '@umet.com.ar'===\n")
