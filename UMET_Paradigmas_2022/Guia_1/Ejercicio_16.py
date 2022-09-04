d4727ec15a6d1f22ead9ae1f26e980cb46dd02ea
Th0m4s <thomasdavalos@outlook.com>
2022-09-04 17:09:08 -0300

    Resuelto

--- UMET_Paradigmas_2022/Guia_1/Ejercicio_16.py
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_16.py
@@ -1,9 +1,14 @@
+#Declaracion de variables
 codigo = 0
 cantidad_horas = 0
 
+
+#Menu y entrada de dato
 print("=======JORNADAS DE TRABAJO=======\n1. Turno diurno (Salario: 35.50$) \n2.Turno nocturno (Salario: 40.60$)\n");
 codigo = int(input("En que turno trabajó el operario?: "))
 
+
+#Control de datos
 while(codigo < 1 or codigo > 2):
 
     print("\n¡Error!, Valor introducido no valido\n");
@@ -12,18 +17,25 @@ while(codigo < 1 or codigo > 2):
 
     codigo = int(input("En que turno trabajó el operario?: "))
 
+
+#Condicional de la varibale 'codigo'
 if(codigo == 1):
 
     cantidad_horas = int(input("Cantidad de horas trabajadas? (Turno diurno): "));
 
+    #Contol de datos
     while(cantidad_horas < 0):
 
         print("\n¡Error! solo valores positivos\n")
 
         cantidad_horas = int(input("Cantidad de horas trabajadas? (Turno diurno): "));
 
+
+    #Imprime la conversion horas * salario
     print(f"Total a pagar: {round(cantidad_horas * 35.50,2)}$")
 
+
+#Mismo caso que la primera condición
 elif(codigo == 2):
 
     cantidad_horas = int(input("Cantidad de horas trabajadas? (Turno nocturno): "));
