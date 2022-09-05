a0c90eb1f36df552ad65077bcc0c05197f1cb958
Th0m4s <thomasdavalos@outlook.com>
2022-09-05 16:28:54 -0300

    Resuelto

--- UMET_Paradigmas_2022/Guia_1/Ejercicio_11.py
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_11.py
@@ -1,22 +1,38 @@
 
-mes = int
+#Declaracion de variables
+mes = 0
+x = 0
 cantidad_ahorro = 0.0
-porcentaje = 0.0
 cantidad_restada = 0.0
+porcentaje = 0.0
+total_ahorrado = 0.0
 
 
+#Entrada de mes
 mes = int(input("En que mes estamos?: "))
 
+
+#Control de datos
 while(mes < 1 and mes > 12):
-    print("\n---Error: Elegir mes por numero (1-12)---");
+    print("\n---Error: Elegir mes comprendido entre (1-12)---");
     mes = int(input("En que mes estamos?: "))
 
-cantidad_ahorro = float(input("Cantidad de pesos a ahorrar: "));
 
-for mes in range(mes,12):
+#Entrada de los ahorros mensuales
+cantidad_ahorro = float(input("Cantidad de pesos a ahorrar mensualmente: "));
 
-    porcentaje += cantidad_ahorro - (cantidad_ahorro * 0.10)
-    
-print(f"{porcentaje}")
-print(f"Cantidad de ahorro para diciembre: {cantidad_ahorro}")
-print(f"Cantidades mensuales restadas: {cantidad_restada}")
+
+#Este for sirve para que desde el mes introducido hasta el mes 12, 'total_ahorrado' se concatene periodicamente
+# con cantidad de ahorro - 10%, este porcentaje restado tambien se mostrará al finalizar el programa.
+for x in range(mes,12):
+
+    cantidad_restada += ((cantidad_ahorro * 10) / 100)
+
+    total_ahorrado += cantidad_ahorro - ((cantidad_ahorro * 10) / 100);
+
+
+
+#Salida de datos
+print(f"Cantidad de ahorro para diciembre: {total_ahorrado}$ (Cantidad restada desde mes ({mes}) hasta mes (12): -{cantidad_restada}$)");
+
+
