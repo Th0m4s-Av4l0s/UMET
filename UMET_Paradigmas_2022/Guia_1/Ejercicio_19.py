7def3d9c30968b22e901048f39bb6284b70b5590
Th0m4s <thomasdavalos@outlook.com>
2022-09-05 16:26:44 -0300

    Incompleto

--- UMET_Paradigmas_2022/Guia_1/Ejercicio_19.py
+++ UMET_Paradigmas_2022/Guia_1/Ejercicio_19.py
@@ -4,6 +4,7 @@ segunda_formula = 0.0
 tercera_formula = 0.0
 control_votos = 100.0
 formula_electa = 0
+eleccion_final = 0
 
 
 #Titulo
@@ -83,8 +84,12 @@ else:
 
 
 
-print(f"{primera_formula} | {segunda_formula} | {tercera_formula}");
+print(f"\nResultados: {primera_formula} | {segunda_formula} | {tercera_formula}");
 
+
+
+#Dependiendo el valor de la variable 'formula_electa' la
+# condicion imprime la formula electa
 if(formula_electa == 1):
 
     print(f"La primera formula quedó electa con {primera_formula}% de los votos.");
@@ -97,17 +102,150 @@ elif(formula_electa == 3):
 
     print(f"La tercera formula quedó electa con {tercera_formula}% de los votos.");
 
-elif(primera_formula and segunda_formula >= 40.0):
+
+
+#En caso de un empate de 2 formulas con 40.0% de votos, se
+# realiza de nuevo una votacion para definir la formula
+# electa
+elif(primera_formula >= 40.0 and segunda_formula >= 40.0):
+
 
     control_votos = 100.0;
 
-elif(primera_formula and tercera_formula >= 40.0):
+    print("\n--Empate entra la primera y segunda formula.--")
+
+    eleccion_final = int(input("\n¿A quien votarás? \n1. Primera formula \n2. Segunda formula \nEleccion: "));
+
+
+
+    while(eleccion_final < 1 or eleccion_final > 2):
+        print("\n¡Error! Opcion no valida, comprendida en (1-2).")
+
+        print("\n--Empate entra la primera y segunda formula.--")
+
+        eleccion_final = int(input("\n¿A quien votarás? \n1. Primera formula \n2. Segunda formula \nEleccion: "));
+
+
+
+    if(eleccion_final == 1):
+
+        print(f"\nCantidad de personas aún sin votar (Segunda vuelta): {control_votos}")
+
+        primera_formula = int(input("\nCantidad de votos?: "));
+
+    elif(eleccion_final == 2):
+
+        print(f"\nCantidad de personas aún sin votar (Segunda vuelta): {control_votos}")
+
+        primera_formula = int(input("\nCantidad de votos?: "));
+
+    
+    if(primera_formula > segunda_formula):
+
+        print("La primera formula quedó electa en la segunda vuelta");
+
+    elif(segunda_formula > primera_formula):
+
+        print("La segunda formula quedó electa en la segunda vuelta");
+
+    else:
+        
+        print("Otro empate electoral.");
+
+
+
+    
+
+elif(primera_formula >= 40.0 and tercera_formula >= 40.0):
 
     control_votos = 100.0;
 
-elif(segunda_formula and tercera_formula >= 40.0):
+    print("\n--Empate entra la primera y tercera formula.--")
+
+    eleccion_final = int(input("\n¿A quien votarás? \n1. Primera formula \n3. Tercera formula \nEleccion: "));
+
+
+
+    while(eleccion_final < 1 or eleccion_final > 3 or eleccion_final == 2):
+        print("\n¡Error! Opcion no valida, comprendida en (1 ó 3).")
+
+        print("\n--Empate entra la primera y Tercera formula.--")
+
+        eleccion_final = int(input("\n¿A quien votarás? \n1. Primera formula \n3. Tercera formula \nEleccion: "));
+
+
+
+    if(eleccion_final == 1):
+
+        print(f"\nCantidad de personas aún sin votar (Segunda vuelta): {control_votos}")
+
+        primera_formula = int(input("\nCantidad de votos?: "));
+
+    elif(eleccion_final == 3):
+
+        print(f"\nCantidad de personas aún sin votar (Segunda vuelta): {control_votos}")
+
+        tercera_formula = int(input("\nCantidad de votos?: "));
+
+    
+    if(primera_formula > tercera_formula):
+
+        print("La primera formula quedó electa en la segunda vuelta");
+
+    elif(tercera_formula > primera_formula):
+
+        print("La segunda formula quedó electa en la segunda vuelta");
+
+    else:
+        
+        print("Otro empate electoral.");
+
+
+
+elif(segunda_formula >= 40.0 and tercera_formula >= 40.0):
 
     control_votos = 100.0;
+
+    print("\n--Empate entra la segunda y tercera formula.--")
+
+    eleccion_final = int(input("\n¿A quien votarás? \n2. Segunda formula \n3. Tercera formula \nEleccion: "));
+
+
+
+    while(eleccion_final < 2 or eleccion_final > 3):
+        print("\n¡Error! Opcion no valida, comprendida en (2-3).")
+
+        print("\n--Empate entra la segunda y tercera formula.--")
+
+        eleccion_final = int(input("\n¿A quien votarás? \n2. Segunda formula \n3. Tercera formula \nEleccion: "));
+
+
+
+    if(eleccion_final == 2):
+
+        print(f"\nCantidad de personas aún sin votar (Segunda vuelta): {control_votos}")
+
+        segunda_formula = int(input("\nCantidad de votos?: "));
+
+    elif(eleccion_final == 3):
+
+        print(f"\nCantidad de personas aún sin votar (Segunda vuelta): {control_votos}")
+
+        tercera_formula = int(input("\nCantidad de votos?: "));
+
+    
+    if(segunda_formula > tercera_formula):
+
+        print(f"La Segunda formula quedó electa en la segunda vuelta con {segunda_formula} de los votos");
+
+    elif(tercera_formula > segunda_formula):
+
+        print(f"La tercera formula quedó electa en la segunda vuelta con {tercera_formula} de los votos");
+
+    else:
+        
+        print("Otro empate electoral.");
+
 
 
     
